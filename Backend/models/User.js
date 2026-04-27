4f5d2cba329c67e930f2cab9c036b76cf50af210
yash-khandelwal-codr <yash.khandelwal.codr@gmail.com>
2026-04-27 11:00:27 +0530

    add user model with password hashing and comparison method

---
+++ Backend/models/User.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: [2, "Name must be atleast 2 character long"],
+    maxlength: [50, "Name cannot exceed 50 characters"],
+  },
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please provide a valid email",
+    ],
+  },
+  password: {
+    type: String,
+    select: false,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be atleast 6 characters long"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    immutable: true,
+  },
+});
+
+userSchema.pre('save', async function () {
+    if(!this.isModified('password')){
+        return
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+userSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        return await bcrypt.compare(candidatePassword, this.password)
+    } catch (error) {
+        throw new Error(error);
+    }
+}   
+
+const User = mongoose.model('User', userSchema)
