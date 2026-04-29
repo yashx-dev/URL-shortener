1764687d795b5cb0b6977d78d35f3ef72599ffca
yash-khandelwal-codr <yash.khandelwal.codr@gmail.com>
2026-04-29 15:44:11 +0530

    add request validation for auth and profile routes

---
+++ Backend/middleware/validationMiddleware.js
@@ -0,0 +1,46 @@
+import { body } from "express-validator";
+
+const registerValidation = [
+  body("name")
+    .isLength({ min: 2 })
+    .withMessage("Name must be atleast 2 characters")
+    .trim()
+    .notEmpty()
+    .withMessage("Name is required"),
+  body("email")
+    .isEmail()
+    .withMessage("Please provide a valid email")
+    .normalizeEmail(),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters")
+    .matches(/\d/)
+    .withMessage("Password must contain a number"),
+];
+
+const loginValidation = [
+  body("email")
+    .isEmail()
+    .withMessage("Please provide a valid email")
+    .normalizeEmail(),
+  body("password").notEmpty().withMessage("Password is required"),
+];
+
+const updateProfileValidation = [
+  body("name")
+    .optional({ checkFalsy: true })
+    .isLength({ min: 2 })
+    .withMessage("Name must be at least 2 characters")
+    .trim(),
+  body("email")
+    .optional({ checkFalsy: true })
+    .isEmail()
+    .withMessage("Please provide a valid email")
+    .normalizeEmail(),
+  body("password")
+    .optional({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters"),
+];
+
+export { registerValidation, loginValidation, updateProfileValidation };
